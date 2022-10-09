907c217e55a69045659781462eb0eac969673d76
omkar3602 <73201152+omkar3602@users.noreply.github.com>
2022-10-09 11:39:19 +0530

    Add task graph api

--- routes/task.js
+++ routes/task.js
@@ -10,8 +10,28 @@ router.route('/task/:id').get((req, res) => {
 });
 
 router.route('/emp/:id').get((req, res) => {
-    Task.find({ emp: req.params.id }).then(task => res.json(task)).catch(err => res.status(400).json('Error: ' + err));
+    Task.find({ emp: req.params.id }).then(tasks => res.json(tasks)).catch(err => res.status(400).json('Error: ' + err));
 });
 
+router.route('/graph/:id').get((req, res) => {
+    Task.find({ emp: req.params.id }).then(tasks => {
+        break_count = 0;
+        meeting_count = 0;
+        work_count = 0;
+        tasks.forEach(task => {
+            if (task.type == 'break') {
+                break_count += task.time_taken;
+            }
+            else if (task.type == 'meeting') {
+                meeting_count += task.time_taken;
+            }
+            else if (task.type == 'work') {
+                work_count += task.time_taken;
+            }
+
+        });
+        res.json({ 'break': Number(break_count / 60), 'meeting': Number(meeting_count / 60), 'work': Number(work_count / 60) });
+    }).catch(err => res.status(400).json('Error: ' + err));
+});
 
 module.exports = router;
